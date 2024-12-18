84860380da59e24d3e04ece0f26c9dcc09e099b5
LiprodCodez <turtle.script.dev@gmail.com>
2024-12-18 18:27:29 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+app.use(express.static("public"));
+
+server.listen(8080, () => {
+  console.log("Server is listening on http://localhost:8080");
+});
