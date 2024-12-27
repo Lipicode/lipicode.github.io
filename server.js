c92f3a7ba74139735fe68faa1cc165e632f6e34e
LiprodCodez <turtle.script.dev@gmail.com>
2024-12-27 12:54:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,21 +5,17 @@ const fetch = require("node-fetch");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set the target server (this is where you want to proxy your requests)
-const targetServer = 'https://example-proxy-server.com'; // Replace this with CroxyProxy or your private server URL
+const targetServer = 'https://example-proxy-server.com';
 
-// Serve the static files
 app.use(express.static("public"));
 
-// Proxy requests
 app.use("/proxy", createProxyMiddleware({
   target: targetServer,
   changeOrigin: true,
   pathRewrite: {
-    '^/proxy': '', // Remove '/proxy' from the URL path
+    '^/proxy': '',
   },
   onProxyReq: (proxyReq, req, res) => {
-    // You can modify the request here if needed, for example, add headers
     console.log(`Proxying request to: ${targetServer}${req.url}`);
   },
   onError: (err, req, res) => {
@@ -28,7 +24,6 @@ app.use("/proxy", createProxyMiddleware({
   }
 }));
 
-// Catch all other requests
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
